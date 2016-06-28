5987b482e4d44709e464298d36ad9a88e8d7516f
Xianguang-Zhou <xianguang.zhou@outlook.com>
2016-06-28 23:01:53 +0800

    modify copyright notice

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 #-*- encoding: utf-8 -*-
-# Copyright (C) 2015 Xianguang Zhou <xianguang.zhou@outlook.com>
+# Copyright (C) 2016 Xianguang Zhou <xianguang.zhou@outlook.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
